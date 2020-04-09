eb2d109a4dd2693d4018491ce5bd859f79c55fcb
Kumar Rahul <blueraaga@gmail.com>
2020-04-09 09:05:27 +0200

    build: move from Go 1.12.7 to 1.14.2

--- bootstrap/core.sh
+++ bootstrap/core.sh
@@ -7,18 +7,20 @@ sudo apt-get -y upgrade
 echo "--- Installing Go ---"
 # Change to Apps directory for all app resource download
 echo "Downloading, extracting and linking Go"
+mkdir -p /vagrant/resources/apps
 cd /vagrant/resources/apps
-sudo wget --no-verbose --timestamping https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz 
-
-mkdir -p go1.12.7
-sudo tar --extract --skip-old-files --file /vagrant/resources/apps/go1.12.7.linux-amd64.tar.gz -C ./go1.12.7
+sudo wget --no-verbose --timestamping https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
+mkdir -p go1.14.2
+sudo tar --extract --skip-old-files --file /vagrant/resources/apps/go1.14.2.linux-amd64.tar.gz -C ./go1.14.2
 
 # mkdir -p /usr/local/go is not required as detailed here:
 # https://askubuntu.com/questions/600714/creating-a-symlink-from-one-folder-to-another-with-different-names/600732
-sudo ln --symbolic --force /vagrant/resources/apps/go1.12.7/go /usr/local/go
+sudo ln --symbolic --force /vagrant/resources/apps/go1.14.2/go /usr/local/go
 
 ## Go Settings
 
+
+sudo echo ' ' >> /etc/profile.d/go-setup.sh
 # 1. Set up bin location for Go
 # as mentioned here: https://golang.org/doc/install
 echo "Setting up bin location for Go"
