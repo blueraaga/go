5689482944293abdd390797db01736b4701028a7
Kumar Rahul <blueraaga@gmail.com>
2020-04-14 08:48:24 +0000

    feat: add function for multiline and variables

--- go/src/s5/s5.go
+++ go/src/s5/s5.go
@@ -13,11 +13,24 @@ func about_handler(w http.ResponseWriter, r *http.Request)  {
 }
 
 
+func work_handler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "<h1>With tags and vars</h1>")
+	
+	fmt.Fprintf(w,
+		`<p>Same fprintf</p>
+		<p>can do multiline</p>
+		<p>texts also.</p>`)
+
+	fmt.Fprintf(w, "I can also print %d which was passed as a %s.", 5, "variable")
+}
+
+
 func main() {
 	fmt.Println("Starting server. Listening at port 8000...")
 	// request redirector to handler function
 	http.HandleFunc("/", index_handler)
 	http.HandleFunc("/about", about_handler)
+	http.HandleFunc("/work", work_handler)
 	// Server listner
 	http.ListenAndServe(":8000", nil)
 }
